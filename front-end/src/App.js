065723e9b72396eba4c8dd82a2e1f5e3f062c7be
jj888 <jaconjcondes@gmail.com>
2019-08-22 15:01:38 -0700

    redone the index and App.js to standard practice

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 import Mail from './containers/Mail';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-import selectMenuActions from './redux/actions/selectMenu.action';
+// import selectMenuActions from './redux/actions/selectMenu.action';
 
 import './App.css';
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <Mail />
-      </div>
+      </Provider>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  currentMenu: state.SelectedMenu.menu,
-  validCredentials: state.Profile.validCredentials
-});
+// const mapStateToProps = (state) => ({
+//   currentMenu: state.SelectedMenu.menu,
+//   validCredentials: state.Profile.validCredentials
+// });
 
-const mapDispatchToProps = {
-  changeMenu: selectMenuActions.selectMenu
-};
+// const mapDispatchToProps = {
+//   changeMenu: selectMenuActions.selectMenu
+// };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(App);
+
+export default App;
